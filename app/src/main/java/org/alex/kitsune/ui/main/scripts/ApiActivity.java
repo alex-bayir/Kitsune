9498990497084680f750fafd4dfd4527fb52f029
alex-bayir <bayir678@gmail.com>
2023-09-09 19:18:29 +0300

    add api page

--- app/src/main/java/org/alex/kitsune/ui/main/scripts/ApiActivity.java
+++ app/src/main/java/org/alex/kitsune/ui/main/scripts/ApiActivity.java
@@ -2,16 +2,20 @@ package org.alex.kitsune.ui.main.scripts;
 
 import android.os.Bundle;
 import android.view.Gravity;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import org.alex.kitsune.Activity;
 import androidx.appcompat.widget.Toolbar;
 import org.alex.kitsune.R;
+import org.alex.kitsune.utils.NetworkUtils;
+import java.io.IOException;
+
+import static org.alex.kitsune.utils.NetworkUtils.*;
 
 public class ApiActivity extends Activity {
     Toolbar toolbar;
-    TextView text;
+    WebView web;
     @Override public int getAnimationGravityIn(){return Gravity.END;}
     @Override public int getAnimationGravityOut(){return Gravity.START;}
     @Override
@@ -21,40 +25,16 @@ public class ApiActivity extends Activity {
         toolbar=findViewById(R.id.toolbar);
         initActionBar(toolbar);
         toolbar.setTitle("API");
-        text=findViewById(R.id.text);
-        text.setHorizontallyScrolling(true);
-        text.setText(getText());
-    }
-    private String getText(){
-        return "API для скриптов (примеры скриптов смотрите в разделе редактирования):\n" +
-                "(написано криво (я самоучка и читать не люблю, да и вряд-ли вы напишите скрипт) так что ориентируйтесь на примеры)\n"+
-                "(Если возникнут трудности можете писать в группу в вк в беседу скриптов и API)\n" +
-                "(так же принимаются предложения о том что добавить или как улучшить приложение)\n"+
-                "Первым делом вам необходимо определить глобальные переменные:\n" +
-                "version – версия скрипта\n" +
-                "domain – провайдер (например mangalib.me)\n" +
-                "source – имя провайдера (например MangaLib)\n" +
-                "Type – тип (например Manga или Ranobe)\n" +
-                "description – описание ресурса\n" +
-                "auth_tokens – токены для авторизации\n"+
-                "\n" +
-                "Дальше необходимо реализовать функцию query, чтобы осуществлять поиск манги и получать по ним основные сведения такие как ссылка на основную страницу данной манги(для функции update), название, рейтинг, описание и т.д. Все эти данные запихиваете в ассоциативный массив и возвращаете список таких массивов.\n" +
-                "List<Map<String,Object>> query(String name, int page, Options[] params) //params нужен для расширенного поиска\n" +
-                "\n" +
-                "Дальше необходимо реализовать функцию query_url, чтобы осуществлять поиск манги по уже готовой ссылке и получать по ним основные сведения такие как ссылка на основную страницу данной манги(для функции update), название, рейтинг, описание и т.д. Все эти данные запихиваете в ассоциативный массив и возвращаете список таких массивов.\n" +
-                "List<Map<String,Object>> query(String url, int page)\n" +
-                "\n" +
-                "Дальше необходимо реализовать функцию update, чтобы обновлять данные по манге (точно так же как и в функции query так же получаете и запихиваете данные в объект Wrapper, только теперь ещё и основные данные по главам и вернуть нужно только один такой объект), а также получать данные о похожей манге (если есть возможность, не загружая другую страницу, а для таких случаев можно реализовать другую функцию которая указана ниже).\n" +
-                "Map<String,Object> update(String url)\n" +
-                "\n" +
-                "Дальше необходимо реализовать функцию getPages, чтобы получать ссылки на страницы (изображения) главы, вернуть нужно список объектов Page.\n" +
-                "List<Page> getPages(String url,Chapter chapter)\n" +
-                "\n" +
-                "Для расширенного поиска нужно реализовать функцию createAdvancedSearchOptions, которая будет возвращать список объектов Options.\n" +
-                "List<Options> createAdvancedSearchOptions()\n" +
-                "\n" +
-                "Для загрузки данных похожей манги можете реализовать функцию loadSimilar, которая будет возвращать ArrayList<Wrapper> (если в функции update не возможно получить эти данные не загружая другую страницу).\n" +
-                "List<Map<String,Object>> loadSimilar(Map<String,Object> manga)\n" +
-                "\n"+"Весь проект вместе с примерами скриптов можно посмотреть на github: https://github.com/alex-bayir/Kitsune";
+        web=findViewById(R.id.web_view);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        new Thread(()->{
+            try {
+                String answer=NetworkUtils.getString("https://raw.githubusercontent.com/alex-bayir/Kitsune/master/content/Scripts%20API.html",getHeadersDefault("github.com",null));
+                NetworkUtils.getMainHandler().post(()-> web.loadDataWithBaseURL(null,answer,"text/html", "UTF-8",null));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
